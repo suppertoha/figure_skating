e55d64a7688e034694fe2829e02ec4081d7dc3df
Toxa <suppertoha@yandex.ru>
2023-04-26 15:18:53 +0300

    new-page

--- src/js/components/accordion.js
+++ src/js/components/accordion.js
@@ -1,23 +1,25 @@
-document.querySelectorAll(".accordion-start-js").forEach((el) => {
-  el.addEventListener("click", () => {
-    el.classList.toggle("open");
-    let content = el.nextElementSibling;
-    if (content.classList.contains("open")) {
-      console.log("test");
-      document
-        .querySelectorAll(".accordion-content-js")
-        .forEach((el) => (el.style.maxHeight = null));
-      document
-        .querySelectorAll(".accordion-content-js")
-        .forEach((el) => content.classList.remove("open"));
-    } else {
-      document
-        .querySelectorAll(".accordion-content-js")
-        .forEach((el) => (el.style.maxHeight = null));
-      //content.style.maxHeight = content.scrollHeight + "px";
-      document
-        .querySelectorAll(".accordion-content-js")
-        .forEach((el) => content.classList.add("open"));
-    }
+if (document.querySelectorAll(".accordion-start-js").length > 0) {
+  document.querySelectorAll(".accordion-start-js").forEach((el) => {
+    el.addEventListener("click", () => {
+      el.classList.toggle("open");
+      let content = el.nextElementSibling;
+      if (content.classList.contains("open")) {
+        console.log("test");
+        document
+          .querySelectorAll(".accordion-content-js")
+          .forEach((el) => (el.style.maxHeight = null));
+        document
+          .querySelectorAll(".accordion-content-js")
+          .forEach((el) => content.classList.remove("open"));
+      } else {
+        document
+          .querySelectorAll(".accordion-content-js")
+          .forEach((el) => (el.style.maxHeight = null));
+        //content.style.maxHeight = content.scrollHeight + "px";
+        document
+          .querySelectorAll(".accordion-content-js")
+          .forEach((el) => content.classList.add("open"));
+      }
+    });
   });
-});
+}
